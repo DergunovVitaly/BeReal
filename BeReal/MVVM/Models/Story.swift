72fd2a1c664ef05908783cc83ecc0349e114d75a
VitaliyD <dergunov.vitaly@gmail.com>
2025-03-12 11:56:24 +0200

    Added UserDefaults need to change in SwiftData

--- BeReal/MVVM/Models/Story.swift
+++ BeReal/MVVM/Models/Story.swift
@@ -4,11 +4,19 @@
 //
 //  Created by Vitaliy on 12.03.2025.
 //
+import Foundation
 
 struct Story: Identifiable, Hashable {
     let id: Int
     let name: String
     let profilePictureURL: String
-    var isSeen: Bool = false
-    var isLiked: Bool = false
+    var isSeen: Bool {
+        get { UserDefaults.standard.bool(forKey: "seen_\(id)") }
+        set { UserDefaults.standard.set(newValue, forKey: "seen_\(id)") }
+    }
+    
+    var isLiked: Bool {
+        get { UserDefaults.standard.bool(forKey: "liked_\(id)") }
+        set { UserDefaults.standard.set(newValue, forKey: "liked_\(id)") }
+    }
 }
--- BeReal/MVVM/Models/UsersResponse.swift
+++ BeReal/MVVM/Models/UsersResponse.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Vitaliy on 12.03.2025.
 //
+import Foundation
 
 struct UsersResponse: Decodable {
     let pages: [Page]
@@ -20,15 +21,5 @@ struct User: Decodable {
     
     private enum CodingKeys: String, CodingKey {
         case id, name, profilePictureURL = "profile_picture_url"
-    }
-    
-    var isSeen: Bool {
-        get { UserDefaults.standard.bool(forKey: "seen_\(id)") }
-        set { UserDefaults.standard.set(newValue, forKey: "seen_\(id)") }
-    }
-    
-    var isLiked: Bool {
-        get { UserDefaults.standard.bool(forKey: "liked_\(id)") }
-        set { UserDefaults.standard.set(newValue, forKey: "liked_\(id)") }
     }
 }
